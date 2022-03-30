2670d9f408dc86989fa964d0afa23061d67c4f34
no2pencil <no2pencil@Buggy.akroncdnr.com>
2022-03-30 07:34:26 -0400

    Added up/down notifiction, time, and fixed trailing zero

--- oil.sh
+++ oil.sh
@@ -10,7 +10,19 @@ do
   previous_value=$(echo ${current_value})
   current_value=$(curl --silent ${url} | grep "price-section__current-value" | cut -d'>' -f2 | cut -d'<' -f1)
   if [ "${current_value}" != "${previous_value}" ]; then
-    echo ${current_value}
+    time=$(date +%H:%M)
+    output=$(echo ${current_value})
+    if [ "${current_value}" > "${previous_value}" ]; then
+      # Check for, and correct trailing zero
+      retval=$(echo ${current_value}|cut -d'.' -f2|wc|awk '{print$3}')
+      if [ ${retval} -eq 2 ]; then
+        output=$(echo ${output}0)
+      fi
+      output=$(echo "${output} [  UP  ] [ ${time} ]")
+    else
+      output=$(echo "${output} [ DOWN ] [ ${time} ]")
+    fi
+    echo ${output}
   fi
   sleep ${sleep_value}
 done
