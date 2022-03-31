7cc39fbd30c0a4172c3d1df24c81c6e5644040d7
no2pencil <no2pencil@Buggy.akroncdnr.com>
2022-03-31 11:25:58 -0400

    Fixed bug in if conditional that was redirecting output to a file, added color text

--- oil.sh
+++ oil.sh
@@ -5,6 +5,13 @@ sleep_value=10
 previous_value=0
 current_value=0
 
+# Text Colors
+#red='\033[0;31m'
+#green='\033[0;32m'
+red='\033[0;41m'
+green='\033[0;42m'
+clear='\033[0m'
+
 while [ 1 -ne 2 ] 
 do 
   previous_value=$(echo ${current_value})
@@ -12,17 +19,18 @@ do
   if [ "${current_value}" != "${previous_value}" ]; then
     time=$(date +%H:%M)
     output=$(echo ${current_value})
-    if [ "${current_value}" > "${previous_value}" ]; then
-      # Check for, and correct trailing zero
-      retval=$(echo ${current_value}|cut -d'.' -f2|wc|awk '{print$3}')
-      if [ ${retval} -eq 2 ]; then
-        output=$(echo ${output}0)
-      fi
-      output=$(echo "${output} [  UP  ] [ ${time} ]")
-    else
-      output=$(echo "${output} [ DOWN ] [ ${time} ]")
+    # Check for, and correct trailing zero
+    retval=$(echo ${current_value}|cut -d'.' -f2|wc|awk '{print$3}')
+    if [ ${retval} -eq 2 ]; then
+      output=$(echo ${output}0)
     fi
-    echo ${output}
+    #if [ ${current_value} -gt ${previous_value} ]; then
+    if [ $(bc <<< "${current_value}>${previous_value}") -eq 1 ]; then
+      output=$(echo "${output} [${green}  UP  ${clear}] [ ${time} ]")
+    else
+      output=$(echo "${output} [${red}DOWN${clear}] [ ${time} ]")
+    fi
+    echo -e ${output}
   fi
   sleep ${sleep_value}
 done
